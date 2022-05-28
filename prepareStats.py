e9d884dafe0980aa97906f9b14630175736f4e38
Michal Kašpárek <michal.kasparek@gmail.com>
2022-05-28 20:58:31 +0200

    1/ improved charts, 2/ European/non-European races

--- prepareStats.py
+++ prepareStats.py
@@ -5,24 +5,27 @@ Don't repeat yourself, they say. So this is the stuff that goes in the beginning
 Jupyter Notebooks exploring various F1 statistics. https://github.com/michalkasparek/f1-stats/
 """
 
-import pandas as pd
-import matplotlib
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Let's define some useful lists first
 
 unknownResult = ["nan0n", "NaN", "NA", "inf"]
 crashes = ["Accident", "Collision", "Fatal accident", "Collision damage", "Spun off"]
-Europe = ["Austrian", "Belgian", "British", "Czech", "Danish", "Dutch", "East German", "Finnish", "French", "German", "Hungarian", "Irish", "Italian", "Liechtensteiner", "Monegasque", "Polish", "Portuguese", "Russian", "Spanish", "Swedish", "Swiss"]
-NAmerica = ["American", "Canadian", "Mexican"]
-SAmerica = ["Argentine", "Brazilian", "Chilean", "Colombian", "Uruguayan", "Venezuelan"]
-Asia = ["Chinese", "Indian", "Indonesian", "Japanese", "Malaysian", "Thai", "Hong Kong"]
-Africa = ["Rhodesian", "South African"]
-Oceania = ["Australian", "New Zealander"]
+
+Europe = ["Austria", "Austrian", "Azerbaijan", "Belgian", "Belgium", "British", "Czech", "Danish", "Dutch", "East German", "Finnish", "France", "French", "German", "Germany", "Hungarian", "Hungary", "Irish", "Italian", "Italy", "Liechtensteiner", "Monaco", "Monegasque", "Netherlands", "Polish", "Portugal", "Portuguese", "Russia", "Russian", "Spanish", "Spain", "Sweden", "Swedish", "Swiss", "Switzerland", "Turkey", "UK"]
+NAmerica = ["American", "Canada", "Canadian", "Mexican", "Mexico", "USA"]
+SAmerica = ["Argentina", "Argentine", "Brazil", "Brazilian", "Chilean", "Colombian", "Uruguayan", "Venezuelan"]
+Asia = ["Bahrain", "Chinese", "China", "Hong Kong", "India", "Indian", "Indonesian", "Japan", "Japanese", "Korea", "Malaysia", "Malaysian", "Qatar", "Saudi Arabia", "Singapore", "Thai", "UAE"]
+Africa = ["Morocco", "Rhodesian", "South Africa", "South African"]
+Oceania = ["Australia", "Australian", "New Zealander"]
 multiple = ["American-Italian", "Argentine-Italian"]
+
 streetCircuits = ["Melbourne", "Monte-Carlo", "Montreal", "Valencia", "Marina Bay", "Sochi", "Baku", "Jeddah", "Adelaide", "Phoenix", "Detroit", "Dallas", "Nevada", "California", "Oporto", "Lisbon"]
 
-# Now let's load the stats. Source: https://www.kaggle.com/datasets/rohanrao/formula-1-world-championship-1950-2020
+# Now let's load the stats (source: https://www.kaggle.com/datasets/rohanrao/formula-1-world-championship-1950-2020)
 
 drivers = pd.read_csv(os.path.join("data", "drivers.csv"))
 results = pd.read_csv(os.path.join("data", "results.csv"))
@@ -44,9 +47,10 @@ results = results.merge(constructors, on="constructorId", how="right")
 results["fullname"] = results["forename"] + " " + results["surname"]
 results["date"] = results["date"].str.slice(0,6) + results["year"].astype(str).str.slice(0,4)
 results["date"] = pd.to_datetime(results["date"], format="%d/%m/%Y")
-results["year"] = results["year"].apply(pd.to_numeric, errors = "coerce").astype(int, errors="ignore")
-results["round"] = results["round"].apply(pd.to_numeric, errors = "coerce").astype(int, errors="ignore")
-results["position"] = results["position"].apply(pd.to_numeric, errors = "coerce").astype(int, errors="ignore")
+results["year"] = results["year"].apply(pd.to_numeric, errors = "coerce").astype(np.int64, errors="ignore")
+results["round"] = results["round"].apply(pd.to_numeric, errors = "coerce").astype(np.int64, errors="ignore")
+results["position"] = results["position"].apply(pd.to_numeric, errors = "coerce").astype(np.int64, errors="ignore")
+results["fastestLapSpeed"] = results["fastestLapSpeed"].apply(pd.to_numeric, errors = "coerce").astype(np.int64, errors="ignore")
 
 # Make some new columns
 
@@ -72,3 +76,8 @@ results = results.merge(entries, on = ["driverId"], how = "right")
 wins = results[results["position"] == 1]
 podiums = results[results["position"] < 4]
 top6 = results[results["position"] < 7]
+
+# Finally let's give the plots some swag
+
+plt.style.use('_mpl-gallery')
+plt.rcParams["figure.figsize"] = (20,3)
